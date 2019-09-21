33692274290329d4ee3c1536e35099b85bc5e75e
Larkin Nickle <me@larbob.org>
2019-09-20 21:20:43 -0400

    Add htop port

---
+++ ports/htop/package.sh
@@ -0,0 +1,7 @@
+#!/bin/bash ../../.port.sh
+port=htop
+version=2.2.0
+useconfigure=true
+configopts="--host=x86_64-k1om-linux --disable-unicode"
+files="https://hisham.hm/htop/releases/2.2.0/htop-2.2.0.tar.gz htop-2.2.0.tar.gz 5da790e6addf495edc4d8ed7d69d1f0de085c7ad"
+depends="ncurses"

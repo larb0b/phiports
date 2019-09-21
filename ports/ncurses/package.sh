82c6374ba14e436f96d8cae8a6dc5cfb59b835c2
Larkin Nickle <me@larbob.org>
2019-09-21 17:57:05 -0400

    ncurses: build w/ termlib

--- ports/ncurses/package.sh
+++ ports/ncurses/package.sh
@@ -2,4 +2,5 @@
 port=ncurses
 version=6.1
 useconfigure=true
+configopts="--host=x86_64-k1om-linux --with-termlib"
 files="ftp://ftp.invisible-island.net/ncurses/ncurses-6.1.tar.gz ncurses-6.1.tar.gz 57acf6bc24cacd651d82541929f726f4def780cc"
